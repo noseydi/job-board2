48b400093079d117abd19d70d3a9b8912d31edfb
noseydi <noseydi@gmail.com>
2025-05-31 12:20:09 +0330

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'jobs',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./jobs/jobs-list/jobs-list.component').then(m => m.JobsListComponent),
+      },
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('./jobs/job-detail/job-detail.component').then(m => m.JobDetailComponent),
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./admin/login/login.component').then(m => m.LoginComponent),
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./admin/dashboard/dashboard.component').then(m => m.DashboardComponent),
+      },
+    ],
+  },
+  { path: '', redirectTo: '/jobs', pathMatch: 'full' },
+];
---
+++ src/app/jobs/job-detail/job-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-detail',
+  imports: [],
+  templateUrl: './job-detail.component.html',
+  styleUrl: './job-detail.component.css'
+})
+export class JobDetailComponent {
+
+}
